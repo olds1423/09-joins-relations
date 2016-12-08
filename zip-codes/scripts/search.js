9960061bcff0840589b9c275b6aed61620f42e46
Scott Schmidt <sjschmidt44@gmail.com>
2016-12-07 16:24:29 -0800

    Update starter code for zip codes assignment, and update lab readme

---
+++ zip-codes/scripts/search.js
@@ -0,0 +1,9 @@
+'use strict';
+
+(function(module) {
+
+  // TODO: Write the code to populate your filters, and enable the search queries here in search.js
+  // TODO: You will also interact with the map.js file here
+
+
+})(window)
