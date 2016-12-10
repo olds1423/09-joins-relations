cc695f68ec9390ee75adf7840962d347ac380b14
olds1423 <olds1423@gmail.com>
2016-12-10 13:37:06 -0800

    we got the state filter populating, just need to clean up dupolicates

--- zip-codes/scripts/search.js
+++ zip-codes/scripts/search.js
@@ -1,9 +1,18 @@
 'use strict';
 
 (function(module) {
-
+  webDB.execute(
+    'SELECT state FROM zips;',
+    function(rows){
+      console.log(rows);
+      if (rows.length){
+        rows.forEach(function(a){
+          $('#state-select').append('<option>' + a.state + '</option>');
+        });
+        // we arent inserting into a template, so just adding options?
+      }
+    }
+  );
   // TODO: Write the code to populate your filters, and enable the search queries here in search.js
   // TODO: You will also interact with the map.js file here
-
-
-})(window)
+})(window);
