0cb36a62050d2f625d68d406d202015b94e04ebe
Kaylee Alvarado <kaymay1000@gmail.com>
2016-12-11 21:00:09 -0800

    Got initMap working with city select on state-specific cities and marker landing where expected.

--- zip-codes/scripts/map.js
+++ zip-codes/scripts/map.js
@@ -3,29 +3,30 @@
 function initMap(newMap) {
   // Create a map object and specify the DOM element for display.
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 47.611435, lng: -122.330456},
+    center: newMap,
     scrollwheel: true,
     zoom: 8
   });
-  // $('#city-select').on('change', function() {
-  //   webDB.execute(
-  //     'SELECT loc FROM zips WHERE loc="' + $(this).val(); + '";',
-  //     function(rows)
-  //   if (rows.length){
-  //   console.log($(this).val(), 'city value');
-  //   rows.forEach(function(a){
-  //     $('#city-select').append('<option>' + a.city + '</option>');
-  //
-  //   });
-  // });
 
   var marker = new google.maps.Marker({
     position: newMap,
     map: map
   });
+};
+
+// {lat: 47.611435, lng: -122.330456}
+// $('#city-select').on('change', function() {
+//   webDB.execute(
+//     'SELECT loc FROM zips WHERE loc="' + $(this).val() + '";',
+//     function(rows) {
+//       if (rows.length){
+//         console.log($(this).val(), 'city value');
+//         rows.forEach(function(a){
+//           newMap = a.loc;
+//
+//         });
+//       }
+//     });
+// });
 
   // TODO: Follow the Google Maps API docs to create markers on the map based on the search options on the home page.
-
-
-/// create something that is variable enough to find the selected object in the json, parse it, access the array at the index for the latitude and longitude
-}
--- zip-codes/scripts/search.js
+++ zip-codes/scripts/search.js
@@ -1,5 +1,8 @@
 'use strict';
-
+var currState;
+var starterLat = 47.611435; //sets initial center of map on page load
+var starterLong = -122.330456;
+var starterMap = {lat: starterLat, lng: starterLong};
 (function(module) {
   webDB.execute(
     'SELECT DISTINCT state FROM zips ' +
@@ -11,22 +14,16 @@
           $('#state-select').append('<option>' + a.state + '</option>');
         });
       }
+      initMap(starterMap);
       $('#state-select').on('change', function() {
-        var latitude = 19.8968;
-        var longitude = 155.5828;
-        var newMap = {lat: latitude, lng: longitude};
-        initMap(newMap);
-
+        currState = $(this).val();
         $('#city-select').empty();
-        console.log($(this).val(), 'state value');
-        console.log($(this).val());
         if ($(this).val()) {
           webDB.execute(
             'SELECT DISTINCT city FROM zips WHERE state="' + $(this).val() + '"' +
             ' ORDER BY city DESC;',
             function(rows){
               if (rows.length){
-                console.log($(this).val(), 'state value');
                 rows.forEach(function(a){
                   $('#city-select').append('<option>' + a.city + '</option>');
 
@@ -36,15 +33,31 @@
           );
         }
       });
+      $('#city-select').on('change', function() {
+        webDB.execute(
+          'SELECT state, latitude, longitude FROM zips WHERE city="' + $(this).val() + '";',
+          function(rows) {
+            if (rows.length){
+              rows.forEach(function(a){
+                if (a.state === currState) {
+                  var newMap = {lat: 0, lng: 0};
+                  newMap = {lat: a.latitude, lng: a.longitude};
+                  console.log(newMap, 'this is our new map object');
+                  initMap(newMap);
+                }
+              });
+            }
+          });
+      });
     }
   );
-  // TODO: Write the code to populate your filters, and enable the search queries here in search.js
-  // TODO: You will also interact with the map.js file here
-  // map = new google.maps.Map(document.getElementById("map"), {...});
+    // TODO: Write the code to populate your filters, and enable the search queries here in search.js
+    // TODO: You will also interact with the map.js file here
+    // map = new google.maps.Map(document.getElementById("map"), {...});
 
 
 
-  // Wire up the zip code search to pull data from the DB and log matching objects to the console (while debugging).
-  //   - You will need to write your SQL queries for a direct search of the db using the zip.
-  //   - You have the `webDB.execute()` method from the blog available to you for accessing the DB table for this assignment.
+    // Wire up the zip code search to pull data from the DB and log matching objects to the console (while debugging).
+    //   - You will need to write your SQL queries for a direct search of the db using the zip.
+    //   - You have the `webDB.execute()` method from the blog available to you for accessing the DB table for this assignment.
 })(window);
